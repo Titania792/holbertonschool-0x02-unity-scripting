dbc75655f281fa02f9c5b19be76498ded4a36ce7
Titania792 <darkwarrior792@gmail.com>
2022-12-22 17:03:56 -0300

    finishing

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //public GameObject player;
+    public Transform target;
+    public Vector3 offset;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.position = target.transform.position + new Vector3(target.transform.position.x, 26, target.transform.position.z);
+        transform.position = target.position + offset;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody player;
-    public float speed = 7000f;
+    public float speed = 1000f;
+    private int score = 0;
+    public int health = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,16 +16,45 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         // AddForce(float x, float y, float z)
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             player.AddForce(0, 0, speed * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
             player.AddForce(0, 0, -speed * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             player.AddForce(-speed * Time.deltaTime, 0, 0);
 
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             player.AddForce(speed * Time.deltaTime, 0, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Pickup")
+        {
+            score += 1;
+            Debug.Log("Score: " + score);
+            Destroy(other.gameObject); //removes the coin from the scene.
+            //other.gameObject.SetActive(false); // hides the object in the scene, we can reference it but not see it in scene.
+        }
+        if (other.gameObject.tag == "Trap")
+        {
+            health -= 1;
+            Debug.Log("Health: " + health);
+        }
+        if (other.gameObject.tag == "Goal")
+        {
+            Debug.Log("You win!");
+        }
+    }
+
+    void Update()
+    {
+        if (health == 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // reset scene to their original values
+        }
     }
 }
